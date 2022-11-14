194ab9d2be4b9ef2214e42ef2ac89b148ebc9450
stee1rat <aboymov@gmail.com>
2022-11-15 01:33:23 +0300

    small fix

--- handlers.py
+++ handlers.py
@@ -201,7 +201,7 @@ def quiz(update, context):
     }
 
     context.job_queue.run_once(
-        quiz_finish, 20, name="quiz" + str(chat_id), context=data
+        quiz_finish, 15, name="quiz" + str(chat_id), context=data
     )
 
 
